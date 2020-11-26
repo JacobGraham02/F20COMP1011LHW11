59ab6b691ba351e904ffe2ade6756567f2f7e8d6
Jaret Wright <hkkA223>
2020-11-26 11:01:12 -0500

    Added Search and poster art to the GUI

--- src/Controllers/MovieSearchViewController.java
+++ src/Controllers/MovieSearchViewController.java
@@ -5,7 +5,11 @@ import Utilities.JSONFileUtility;
 import Utilities.OmdbApiUtility;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,15 +21,54 @@ public class MovieSearchViewController implements Initializable {
     @FXML
     private ListView<MovieInfo> movieListView;
 
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Label rowsReturnedLabel;
+
+    @FXML
+    private ImageView imageView;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //add a listener to the ListView so that when a Movie is selected, the
+        //imageView is updated with the poster for that movie
+        movieListView.getSelectionModel().selectedItemProperty().addListener(
+                (obs, oldValue, movieSelected) -> {
+                    try{
+                        imageView.setImage(new Image(movieSelected.getPoster()));
+                    } catch (Exception e)
+                    {
+                        imageView.setImage(new Image("./Views/default-movie.png"));
+                    }
+                }
+        );
+
+        updateLabels();
+    }
+
+    private void updateLabels()
+    {
+        rowsReturnedLabel.setText("Rows Returned: "+movieListView.getItems().size());
+    }
+
+    @FXML
+    private void getMovies()
+    {
+        movieListView.getItems().clear();
         try {
-            OmdbApiUtility.getMovieFromSearch("Rudolph");
+            //replace the space character with %20 so it will work with HTTP
+            String movieName = textField.getText();
+            movieName = movieName.replace(" ", "%20");
+
+            OmdbApiUtility.getMovieFromSearch(movieName);
             movieListView.getItems().addAll(JSONFileUtility.getMovieList("./src/JSONFiles/movieSearch.json"));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        updateLabels();
     }
 }
